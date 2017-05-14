ba020c03707c800ed68a5d09121cd94861765922
Zi Wang <ziwang@uber.com>
2017-05-13 23:19:28 -0400

    class component vesus function component

--- index.ios.js
+++ index.ios.js
@@ -2,12 +2,16 @@
 
 // Import a library to help creating a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
+import AlbumList from './src/components/album-list';
 import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
---
+++ src/components/album-list.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+    // debugger
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
+
